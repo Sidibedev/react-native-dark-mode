a89ec600d130ff637924caf378604f0db24edc74
Mohamed Aly Sidibe <mouhamed.aly.sidibe96@gmail.com>
2020-08-15 19:58:53 +0000

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+  useTheme,
+} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { AppearanceProvider, useColorScheme } from "react-native-appearance";
+
+const Stack = createStackNavigator();
+
+function screen1({ navigation }) {
+  const { colors } = useTheme();
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ color: colors.text }}>I am the screen 1</Text>
+      <Button
+        title="Go to Screen 2"
+        color={colors.primary}
+        onPress={() => navigation.navigate("screen2")}
+      />
+    </View>
+  );
+}
+
+function screen2() {
+  const { colors } = useTheme();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ color: colors.text }}>I am the screen 2</Text>
+    </View>
+  );
+}
 
 export default function App() {
+  const scheme = useColorScheme();
+  const MyDarkTheme = {
+    dark: true,
+    colors: {
+      primary: "#9933FF",
+      background: "#000023",
+      card: "#000028",
+      text: "#ffffff",
+      border: "#000028",
+      notification: "#9933FF",
+    },
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppearanceProvider>
+      <NavigationContainer
+        theme={scheme === "dark" ? MyDarkTheme : DefaultTheme}
+      >
+        <Stack.Navigator mode="modal">
+          <Stack.Screen name="screen1" component={screen1} />
+          <Stack.Screen name="screen2" component={screen2} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AppearanceProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
